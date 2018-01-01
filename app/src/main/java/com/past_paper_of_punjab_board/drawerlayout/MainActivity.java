e9f83138c51dbae269de91b8a4e872f926be523e
immujahidkhan <immujahidkhan6@gmail.com>
2018-01-01 16:16:49 +0500

    Initial commit

---
+++ app/src/main/java/com/past_paper_of_punjab_board/drawerlayout/MainActivity.java
@@ -0,0 +1,58 @@
+package com.past_paper_of_punjab_board.drawerlayout;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mToggle;
+    Toolbar mToolbar;
+    NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationView = findViewById(R.id.navigation);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = findViewById(R.id.drawerlayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.Item1:
+                        Toast.makeText(MainActivity.this, "Item1", Toast.LENGTH_SHORT).show();
+                    case R.id.Item2:
+                        Toast.makeText(MainActivity.this, "Item2", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
